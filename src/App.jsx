da432bf18b9d1bacc439192ccdd266e0d620ab68
Shuvro0421 <adibarmanshuvro89@gmail.com>
2023-09-12 01:23:05 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-5xl text-red-500 font-semibold text-center'>Knowledge Cafe</h1>
+    </>
+  )
+}
+
+export default App
